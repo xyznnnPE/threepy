e8075644ca5b4cb0d641223b29414a3bea74294b
thepipboy <3476647793@qq.com>
2025-05-13 10:10:22 +0800

    Update onetensor.py

--- tensor/clock/onetensor.py
+++ tensor/clock/onetensor.py
@@ -29,12 +29,12 @@ def onetensor():
                    [1, 2, 3]])
     
     onetensor8 = ([[3, 4, 5],
-                  [6, 7, 8],
-                  [9, 1, 2]])
+                   [6, 7, 8],
+                   [9, 1, 2]])
     
     onetensor9 = ([[2, 3, 4],
-                  [5, 6, 7],
-                  [8, 9, 1]])
+                   [5, 6, 7],
+                   [8, 9, 1]])
     
     return [onetensor1,onetensor2,onetensor3,onetensor4,onetensor5,onetensor6,onetensor7,onetensor8,onetensor9]
 print(onetensor)
